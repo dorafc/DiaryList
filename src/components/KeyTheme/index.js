217993f6cc6a357ed8d7ed037a2c89203cf36e8d
Dora Fraeman Caswell <dorafraeman@gmail.com>
2019-06-28 18:48:22 -0400

    initialize state

---
+++ src/components/KeyTheme/index.js
@@ -0,0 +1,36 @@
+
+
+export const colorCodes = {
+	codes : [
+		{
+			name : 'make',
+			text : 'Make / Craft',
+			color : 'black'
+		},
+		{
+			name : 'care',
+			text : 'Self Care',
+			color : 'gray'
+		},
+		{
+			name : 'media',
+			text : 'Media / Arts',
+			color : 'hotpink'
+		},
+		{
+			name : 'community',
+			text : 'Community',
+			color : 'limegreen'
+		},
+		{
+			name : 'experience',
+			text : 'Experience',
+			color : 'yellow'
+		},
+		{
+			name : 'learning',
+			text : 'Learning',
+			color : 'palegreen'
+		}
+	]
+}
