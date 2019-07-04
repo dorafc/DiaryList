9f663945c2d2629aa0f3730be14b08f8ae026917
Dora Fraeman Caswell <dorafraeman@gmail.com>
2019-07-04 13:40:39 -0400

    updating data structure

--- src/components/DisplayEntries/index.js
+++ src/components/DisplayEntries/index.js
@@ -6,10 +6,9 @@ class DisplayEntries extends Component {
 	constructor(props) {
 		super(props);
     this.state = {
-    	test : "beep",
-    	today : [],
     	dateEntries : [],
-    	numDates : 0
+    	numDates : 0,
+    	noteEntries : []
     }
   }
 
@@ -17,62 +16,50 @@ class DisplayEntries extends Component {
   	const db = this.props.firebase.db;
 		const userDb = db.collection('users').doc('dcaswell').collection('dates')
 
-		let dateEntries = [];
+		// let dateEntries = [];
+		// let noteEntries = [];
 
-		let today = new Date()
+		// let today = new Date()
 
 		// get date collections
-		userDb.get()
-		.then((querySnapshot) => {
-			querySnapshot.forEach((doc) => {
-				dateEntries.push(doc.id)
-			})
-			this.setState({
-				dateEntries : dateEntries,
-				numDates : dateEntries.length
-			})
-		}).catch((error) => {
-			console.log("Error getting dates documents: ", error);
-		})
-
-		console.log(dateEntries)
-
-
-		// TODAY
-		userDb.doc(today.toISOString().slice(0,10)).collection('notes').get()
-		.then((querySnapshot) => {
-			let newData = this.state.today.slice()
-			// console.log('TODAY')
-			querySnapshot.forEach((doc) => {
-				// console.log(doc.id, " => ", doc.data());
-				newData.push(doc.data())
-			})
-			this.setState({
-				today : newData
-			})
-		})
-		.catch((error) => {
-			console.log("Error getting documents: ", error);
-		})
-
-		//  YESTERDAY
-		// userDb.doc(yesterday.toISOString().slice(0,10)).collection('notes').get()
+		// userDb.get()
 		// .then((querySnapshot) => {
-		// 	// console.log(querySnapshot)
-		// 	console.log('YESTERDAY')
-		// 	querySnapshot.forEach((doc) => {
-		// 		console.log(doc.id, " => ", doc.data());
+		// 	querySnapshot.forEach((doc, i) => {
+		// 		dateEntries.push(doc.id)
+		// 		let entries = []
+		// 		userDb.doc(doc.id).collection('notes').get()
+		// 		.then((queryNotes) => {
+		// 			queryNotes.forEach((doc) => {
+		// 				// console.log(doc.data())
+		// 				entries.push(doc.data())
+		// 			})
+		// 			noteEntries.push(entries)
+		// 		})
+		// 		.catch((error) => {
+		// 			console.log("Error getting notes documents: ", error);
+		// 		})
 		// 	})
+		// 	console.log(noteEntries[2])
+		// 	this.setState({
+		// 		// dateEntries : dateEntries,
+		// 		numDates : dateEntries.length,
+		// 	})
+		// 	// dateEntries.forEach(foo => console.log(foo))
 		// })
 		// .catch((error) => {
-		// 	console.log("Error getting documents: ", error);
+		// 	console.log("Error getting dates documents: ", error);
 		// })
+
+		//get entries
+		// console.log(this.state.dateEntries)
+		// dateEntries.forEach(foo => console.log(foo))
+
   }
 
 	render(){
 		return (
 			<div className="DisplayEntries">
-				<Day date="Today" entryCount={2} data={this.state.today} />
+				<Day date="Test" data={[{shortText : "Hello, this is some text", theme : 'make'}]} />
 			</div>
 		)
 	}
--- src/components/EditEntry/index.js
+++ src/components/EditEntry/index.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 // import Firebase from '../Firebase';
+import 'firebase/firestore';
+// import 'firebase/functions';
 
 // const entryDate = new Date();
 // const dayEntry = entryDate.toISOString().slice(0,10)
@@ -30,30 +32,37 @@ class EditEntry extends Component {
 
   onSubmit(event){
   	const db = this.props.firebase.db;
-  	const dateDoc = this.state.date.toISOString().slice(0,10);
+  	// const dateDoc = this.state.date.toISOString().slice(0,10);
+  	const dateDoc = new Date(this.state.date.getFullYear(),this.state.date.getMonth(), this.state.date.getDay())
 
   	event.preventDefault();
 
+
+
   	// add new date to collection
-  	db.collection('users').doc('dcaswell').collection('dates').doc(dateDoc).set({date : dateDoc}).then(()=>{
+  	db.collection('users').doc('dcaswell').collection('notes').doc().set({
+  		date : dateDoc,
+  		// notes : this.props.firebase.firestore.FieldValue.arrayUnion(this.state)
+	 	}).then(()=>{
   		console.log('Set new date document!')
   	})
   	.catch(function(error) {
-		    console.error("Error creating new date: ", error);
+		    console.error("Error creating document: ", error);
 		});
 
   	// add new note to notes collection
-  	db.collection('users').doc('dcaswell').collection('dates').doc(dateDoc).collection('notes').doc().set(this.state)
-  	.then(() => {
-		    console.log("Document successfully written!");
-		    this.setState({ ...initialState })
-		})
-		.catch(function(error) {
-		    console.error("Error writing document: ", error);
-		});
+  // 	db.collection('users').doc('dcaswell').collection('dates').doc(dateDoc).collection('notes').doc().set(this.state)
+  // 	.then(() => {
+		//     console.log("Document successfully written!");
+		//     this.setState({ ...initialState })
+		// })
+		// .catch(function(error) {
+		//     console.error("Error writing document: ", error);
+		// });
   }
 
 	render(){
+		// console.log(firebase.firestore.FieldValue)
 		return (
 			<div className="EditEntry">
 				<form onSubmit={this.onSubmit}>
