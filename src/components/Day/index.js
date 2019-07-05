49533af2576fa8d3d9f4757b626f0924f2711c26
Dora Fraeman Caswell <dorafraeman@gmail.com>
2019-07-05 09:50:05 -0400

    updated Day and Entry components

--- src/components/Day/index.js
+++ src/components/Day/index.js
@@ -1,22 +1,66 @@
 import React, {Component} from 'react';
 
 import Entry from '../Entry'
-// import AddEntry from '../AddEntry'
 
 class Day extends Component {
+	constructor(props) {
+		super(props);
+    this.state = {
+    	notes : [],
+    	day : ''
+    }
+	}
+
+	componentDidMount(){
+  	const db = this.props.firebase.db;
+		const userDb = db.collection('users').doc('dcaswell').collection('dates').doc(this.props.day).collection('notes')
+
+		// get notes collections
+		userDb.get()
+		.then((querySnapshot) => {
+			let notes = []
+			querySnapshot.forEach((note) => {
+				notes.push(note.data())
+			})
+			return notes
+		})
+		.then((data) => {
+			const date = data[0].date.toDate()
+			const day = new Date(date.getFullYear(), date.getMonth(), date.getDay()).toDateString()
+			const today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDay()).toDateString()
+			let setDay;
+
+			if (day === today.toString()){
+				setDay = 'Today'
+			} else {
+				setDay = day
+			}
+
+			this.setState({
+				notes : data,
+				day : setDay
+			})
+		})
+		.catch((error) => {
+			console.log("Error getting notes documents: ", error);
+		})
+
+	}
 
 	render(){
+		const entryList = this.state.notes.slice().map((note, i) => {
+			return(
+				<Entry shortText={note.shortText} theme={note.theme} longText={note.longText} key={"note" + i} />
+			)
+		})
 
-		// const entryList = this.props.data.map((info, i) => 
-			// <Entry key={"entry"+i} entry={info} />
-		// )
 		return (
 			<div className="day">
-				<DateHeader date={this.props.day} />
+				<DateHeader date={this.state.day} />
 				{/*this.props.date === 'Today' &&
 					<AddEntry />*/
 				}
-				<ul>{/*entryList*/}</ul>
+				<ul>{entryList}</ul>
 				<hr />
 			</div>
 		)
--- src/components/Entry/index.js
+++ src/components/Entry/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Entry(props){
 	// console.log(props.entry)
 	return(
-		<li className={"entry "+props.entry.theme}>
-			{props.entry.shortText}
+		<li className={"entry "+props.theme}>
+			{props.shortText}
 		</li>
 	)
 }
