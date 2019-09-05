1a03a274bff31d02e00583c283af0802a6062440
Dora Fraeman Caswell <dorafraeman@gmail.com>
2019-09-05 12:43:32 -0400

    cleaned up unused code from DisplayEntries

--- src/components/DisplayEntries/index.js
+++ src/components/DisplayEntries/index.js
@@ -7,7 +7,6 @@ class DisplayEntries extends Component {
 	constructor(props) {
 		super(props);
     this.state = {
-			dateEntries : [],
 			queryDocs : []
     }
   }
@@ -19,20 +18,8 @@ class DisplayEntries extends Component {
     // get date collections
     userDb.get()
     .then((querySnapshot) => {
-			let datesIds = [];
-
 			this.setState({
 				queryDocs : querySnapshot.docs
-			})
-
-      querySnapshot.forEach((doc) => {
-        datesIds.push(doc.id)
-			})
-      return datesIds
-    })
-    .then((data) => {
-      this.setState({
-        dateEntries : data
 			})
     })
     .catch((error) => {
@@ -41,42 +28,7 @@ class DisplayEntries extends Component {
   }
 
 	render(){
-
-		// const days = this.state.dateEntries.slice().map((date) => {
-		// 	// calc date labels
-		// 	let label;
-
-		// 	if (date === today){
-		// 		label = "Today"
-		// 	} else if (date === yesterday){
-		// 		label = "Yesterday"
-		// 	} else {
-		// 		label = new Date(date).toDateString()
-		// 	}
-
-		// 	return(
-		// 		<DayData 
-		// 			day={date} 
-		// 			key={date} 
-		// 			label={label} 
-		// 			onEdit={this.props.onEdit} 
-		// 			showAll={this.props.showAll} 
-		// 			userId={this.props.userId} 
-		// 		/>
-		// 	)
-		// })
-
 		const days = this.state.queryDocs.slice().map((date, i) => {
-			// calc date labels
-			// let label = 'foo';
-
-			// if (date === today){
-			// 	label = "Today"
-			// } else if (date === yesterday){
-			// 	label = "Yesterday"
-			// } else {
-			// 	label = new Date(date).toDateString()
-			// }
 
 			return(
 				<Day 
@@ -102,7 +54,5 @@ class DisplayEntries extends Component {
 		)
 	}
 }
-
-// const DayData = withFirebase(Day)
 
 export default withFirebase(DisplayEntries);
