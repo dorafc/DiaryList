161dfda0832bf84fdc88903bfdc10c660df6ae03
Dora Fraeman Caswell <dorafraeman@gmail.com>
2019-09-11 18:43:17 -0400

    background for loader

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 
+import styled, { keyframes } from 'styled-components'
+
 import SignedOut from '../SignedOut';
 import SignedIn from '../SignedIn';
 import { withFirebase } from '../Firebase';
@@ -27,12 +29,11 @@ class App extends Component {
   }
 
   render(){
-    // const content = (this.state.authUser !== null) ? <SignedInData userId={this.state.authUser.uid} authUser={this.state.authUser} /> : <SignedOutData />
 
     let content;
 
     if (this.state.loading === true){
-      content = <Spinny />
+      content = <Wrapper><Spinny /></Wrapper>
     } else {
       if (this.state.authUser !== null){
         content = <SignedInData userId={this.state.authUser.uid} authUser={this.state.authUser} />
@@ -52,6 +53,25 @@ class App extends Component {
 function Spinny(props){
   return <h1>SPINNING WHEEL OF WAITING</h1>
 }
+
+// styled components
+
+const rainbowKeyframes =  keyframes` 
+  0%{background-position:0% 82%}
+  50%{background-position:100% 19%}
+  100%{background-position:0% 82%}
+`
+const Wrapper = styled.div`
+  margin: 0;
+  padding: 75px 30px 0;
+  min-height: 100vh;
+  background: linear-gradient(90deg, #fceae8, #ffebeb, #fff9e8, #ffffeb, #edfff0, #e8feff, #eeedff, #fdedff, #fdebff);
+  background-size: 1800% 1800%;
+  animation: ${rainbowKeyframes} 30s ease infinite;
+  @media (max-width: 650px){
+    padding: 75px 17px 0;
+  }
+`
 
 // components with data
 const SignedOutData = withFirebase(SignedOut)
